0b790601f37c1962daeb98346a7113f8ef254826
Ivyk <ivyk96@live.com>
2014-12-08 23:34:37 +0100

    Van alles en nog wat aangepast.

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Kate Organizing - Home</title>
+    <?php include_once('includes/head.html') ?>
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <?php include_once('includes/navbar.html') ?>
+
+
+
+    <!-- Page Content -->
+    <div class="container">
+        <br>
+        <div class="row">
+            <div class="col-md-2"><p class="page-header-whitespace">-</p></div>
+            <img class="img-responsive" src="img/banner.png">
+            <div class="col-md-2"><p class="page-header-whitespace">-</p></div>
+        </div>
+        <!-- Features Section -->
+        <div class="row">
+            <div class="text-center">
+                <h3 class="page-header2">prioriTIJD aan kwaliTIJD</h3>
+            </div>
+
+            <br>
+
+            <div class="col-lg-12">
+                <h4 class="page-header3">organiseren kun je leren</h4>
+            </div>
+        </div>
+        <hr>
+        <div class="row"> 
+            <div class="col-md-12">
+                <p>
+                    Dagelijks een stroom aan mailverkeer, stapels post, een overvolle agenda en altijd tijd tekort??
+                    Mensen zijn vaak uitstekend opgeleid voor het uitoefenen van hun vak maar hebben nooit geleerd hoe ze hun werk efficiënt en effectief organiseren.
+                    Er is niet altijd sprake van chaos! Er zijn mensen die alles prima in orde hebben, maar geen (goed) systeem hebben aangebracht. Ook dat kan tot problemen leiden.
+                </p>
+                <p><b>Tijd om het roer om te gooien, om echt kwaliTIJD voor jezelf te krijgen!</b></p>
+                <p>
+                    Cathy Hoekstra is professional organizer. Samen met u brengt zij structuur aan in uw werkzaamheden en op uw werkplek. Ook helpt Kate Organizing met het plannen van de 
+                    agenda en met het indelen van tijd en taken. De training Effectief en Efficiënt werken is gebaseerd op o.a. de principes van "Getting things done" en Steven Covey. De training 
+                    vindt plaats daar waar het nodig is: op de werkplek zelf. De persoonlijke aanpak blijkt tot grote resultaten te leiden.
+                </p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-3 portfolio-item">
+                <h4 class="contact-header-blauw">contact</h4>
+                <p>info@kateorganizing.nl</p>
+                <p>078-6427346</p>
+                <p>Papendrecht</p>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <h4 class="contact-header-oranje">social media</h4>
+                <a href="https://www.linkedin.com/pub/cathy-hoekstra/3/b51/a94" target="_blank"><p><i class="fa fa-linkedin-square"></i> linkedin</p></a>
+                <a href="https://twitter.com/HoekstraCathy" target="_blank"><p><i class="fa fa-twitter-square"></i> twitter</p></a>
+                <a href="https://www.facebook.com/pages/Kate-Organizing/715162075186679?pnref=lhc" target="_blank"><p><i class="fa fa-facebook-square"></i> facebook</p></a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <h4 class="contact-header-blauw">referenties</h4>
+                <p>klik hier voor referenties</p>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <h4 class="contact-header-oranje">NBPO</h4>
+                <img class="img-responsive" src="img/nbpo.png">
+            </div>
+
+        </div>
+        <hr>
+
+        <!-- Footer -->
+        <?php include_once('includes/footer.html') ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
